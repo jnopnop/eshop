bbbc8603f949b6cb3b47f9eae7285ee4da3cee97
jnopnop <jnopnop@users.noreply.github.com>
2014-09-07 22:56:10 +0400

    Created project structure

---
+++ src/main/java/org/nop/eshop/HomeController.java
@@ -0,0 +1,16 @@
+package org.nop.eshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String home(ModelMap model) {
+        model.addAttribute("message", "Welcome to eshop!");
+        return "index";
+    }
+}
