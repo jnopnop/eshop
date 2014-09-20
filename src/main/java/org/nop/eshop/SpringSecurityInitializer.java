9d7300be66c6b98fa878e625e8b631571bcae4c6
jnopnop <jnopnop@users.noreply.github.com>
2014-09-20 18:40:36 +0400

    SpringSecurity configs

---
+++ src/main/java/org/nop/eshop/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.nop.eshop;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //That's enough
+}
